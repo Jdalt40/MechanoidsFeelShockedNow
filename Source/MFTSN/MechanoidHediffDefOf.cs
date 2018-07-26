3a5683f7c977a88864f8869a9e251e8078346bfa
Jdalt40 <Daltonjosh5@gmail.com>
2018-07-26 18:58:03 +1000

    First Upload

---
+++ Source/MFTSN/MechanoidHediffDefOf.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MFTSN
+{
+	[DefOf]
+    public static class MechanoidHediffDefOf
+    {
+        static MechanoidHediffDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(HediffDefOf));
+        }
+        public static HediffDef MechanoidShock;
+    }
+}
---
+++ Source/MFTSN/MechanoidsShortCircuit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MFTSN
+{
+    public class GameCondition_SolarFlare : GameCondition
+    {
+
+        public override void GameConditionTick()
+        {
+            List<Map> affectedMaps = base.AffectedMaps;
+            if (Find.TickManager.TicksGame % 60 == 0)
+            {
+                for (int i = 0; i < affectedMaps.Count; i++)
+                {
+                    this.DoMechanoidDamage(affectedMaps[i]);
+                }
+            }
+        }
+        public void DoMechanoidDamage(Map map)
+        {
+            List<Pawn> allPawnsSpawned = map.mapPawns.AllPawnsSpawned;
+            for (int i = 0; i < allPawnsSpawned.Count; i++)
+            {
+                Pawn pawn = allPawnsSpawned[i];
+                if (pawn.def.race.IsMechanoid)
+                {
+                    float num = 0.005f;
+                    if (num != 0f)
+                    {
+                        float num2 = Mathf.Lerp(0.85f, 1.15f, Rand.ValueSeeded(pawn.thingIDNumber ^ 74374237));
+                        num *= num2;
+                        HealthUtility.AdjustSeverity(pawn, MechanoidHediffDefOf.MechanoidShock, num);
+                    }
+                }
+            }
+        }
+    }
+}
